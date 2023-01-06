bde55a7cd5b915ff2149514428acc826c1988ad1
telecom <neradivoznuk@teco.com.ar>
2023-01-06 09:44:33 -0300

    Elimina Lombok y refactoriza nombres

--- src/main/java/com/mutant/model/Adn.java
+++ src/main/java/com/mutant/model/Adn.java
@@ -3,25 +3,28 @@ package com.mutant.model;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
-
-@Data
-@RequiredArgsConstructor
 public class Adn {
 
-	private String[] adn;
+	private String[] code;
+	
+	public String[] getCode() {
+		return code;
+	}
+
+	public void setAdn(String[] code) {
+		this.code = code;
+	}
 
 	public String toString() {
-		return String.join(",", this.adn);
+		return String.join(",", this.code);
 	}
 	
 	public boolean isEmpty() {
-		return (this.adn.length == 0);
+		return (this.code.length == 0);
 	}
 
 	public Mutant createtMutant(boolean isMutant) {
-		return new Mutant(String.join(",", this.adn), isMutant);
+		return new Mutant(String.join(",", this.code), isMutant);
 	}
 
 	public boolean isValid() {
@@ -30,7 +33,7 @@ public class Adn {
 
 	private boolean isNitrogenBase() {
 		//El ADN sólo puede contener las base nitrogenada del ADN sólo pueden ser: A, T, C y G
-		String adn = String.join(",", this.adn);
+		String adn = String.join(",", this.code);
 		Pattern pattern = Pattern.compile("[ATCG]");
 		Matcher matcher = pattern.matcher(adn);
 		return matcher.matches();
@@ -40,8 +43,8 @@ public class Adn {
 		//El ADN debe ser NxN
 		boolean validStructure = true;
 		int i = 0;
-		while (validStructure && i < this.adn.length) {
-			validStructure = (this.adn[i].length() == this.adn.length);
+		while (validStructure && i < this.code.length) {
+			validStructure = (this.code[i].length() == this.code.length);
 			i++;
 		}
 		return validStructure;
