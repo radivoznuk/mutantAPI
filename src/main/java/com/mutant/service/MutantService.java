579d923e2c3babd84d13be472591f35e57f48113
telecom <neradivoznuk@teco.com.ar>
2023-01-06 09:43:41 -0300

    Quita Lombok

--- src/main/java/com/mutant/service/MutantService.java
+++ src/main/java/com/mutant/service/MutantService.java
@@ -5,17 +5,12 @@ import org.springframework.stereotype.Service;
 import com.mutant.model.Mutant;
 import com.mutant.repository.MutantRepository;
 
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
-
 @Service
-@Data
-@RequiredArgsConstructor
 public class MutantService {
 	//MINIMO_EQUAL_NITORGEN_BASE indica cuantas veces se debe repetir la base nitrogenada para ser un ADN de MUTANTE
 	static final int MINIMO_EQUAL_NITORGEN_BASE = 4; 
 	
-	private final MutantRepository mutantRepository;
+	private MutantRepository mutantRepository;
 
 	public boolean isMutant(String[] adn) {
 		boolean isMutant = false;
@@ -38,9 +33,9 @@ public class MutantService {
 
 	private boolean verticalRepeated(int i, int j, String[] adn) {
 		int countEquals = 1;//cantidad repetidos
-		//Mientras la fila a analizar mas la cantidad necesaria para ser mutante sea menor al tamaño de filas de la matriz
+		//Mientras la fila a analizar más la cantidad necesaria para ser mutante sea menor al tamaño de filas de la matriz
 		//y la cantidad de iguales sea menor a la cantidad de iguales necesarios para ser mutante
-		//y el caracter en la posicion (i,j) es igual al caracter en la posicion (i+countEquals,j)
+		//y el caracter en la posición (i,j) es igual al caracter en la posición (i+countEquals,j)
 		while ((i+MINIMO_EQUAL_NITORGEN_BASE<adn.length) && (countEquals < MINIMO_EQUAL_NITORGEN_BASE) && (adn[i].charAt(j)==adn[i+countEquals].charAt(j))) {
 			countEquals++;
 		}
@@ -49,9 +44,9 @@ public class MutantService {
 
 	private boolean horizontalRepeated(int i, int j, String[] adn) {
 		int countEquals = 1;//cantidad repetidos
-		//Mientras la columna a analizar mas la cantidad necesaria para ser mutante sea menor al tamaño de columnas de la matriz
+		//Mientras la columna a analizar más la cantidad necesaria para ser mutante sea menor al tamaño de columnas de la matriz
 		//y la cantidad de iguales sea menor a la cantidad de iguales necesarios para ser mutante
-		//y el caracter en la posicion (i,j) es igual al caracter en la posicion (i,j+countEquals)
+		//y el caracter en la posición (i,j) es igual al caracter en la posición (i,j+countEquals)
 		while ((j+MINIMO_EQUAL_NITORGEN_BASE<adn[i].length()) && (countEquals < MINIMO_EQUAL_NITORGEN_BASE) && (adn[i].charAt(j)==adn[i].charAt(j+countEquals))) {
 			countEquals++;
 		}
@@ -60,9 +55,9 @@ public class MutantService {
 
 	private boolean diagonalRepeated(int i, int j, String[] adn) {
 		int countEquals = 1;//cantidad repetidos
-		//Mientras la columna y fila a analizar mas la cantidad necesaria para ser mutante sean menor al tamaño de columnas de la matriz
+		//Mientras la columna y fila a analizar más la cantidad necesaria para ser mutante sean menor al tamaño de columnas de la matriz
 		//y la cantidad de iguales sea menor a la cantidad de iguales necesarios para ser mutante
-		//y el caracter en la posicion (i,j) es igual al caracter en la posicion (i+countEquals,j+countEquals)
+		//y el caracter en la posición (i,j) es igual al caracter en la posición (i+countEquals,j+countEquals)
 		while ((i+MINIMO_EQUAL_NITORGEN_BASE<adn.length) && (j+MINIMO_EQUAL_NITORGEN_BASE<adn[i].length()) && (countEquals < MINIMO_EQUAL_NITORGEN_BASE) && (adn[i].charAt(j)==adn[i+countEquals].charAt(j+countEquals))) {
 			countEquals++;
 		}
