72ce537ddda9a426873c0f74235f4d88204b9250
telecom <neradivoznuk@teco.com.ar>
2023-01-06 09:42:50 -0300

    Refactoriza

--- src/main/java/com/mutant/controller/MutantController.java
+++ src/main/java/com/mutant/controller/MutantController.java
@@ -13,32 +13,31 @@ import org.springframework.web.bind.annotation.RestController;
 import com.mutant.model.Adn;
 import com.mutant.model.Mutant;
 import com.mutant.model.Stat;
-import com.mutant.service.MutantService;
 
-import lombok.RequiredArgsConstructor;
+import com.mutant.service.MutantService;
 
 @RestController
 @RequestMapping("/")
-@RequiredArgsConstructor
 public class MutantController {
-	private final MutantService mutantService;
-	private Mutant mutant;
+	private final MutantService mutantService = new MutantService();
 		
 	@PostMapping(value="/mutant/", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-	public ResponseEntity<String> isMutant(@NonNull @RequestBody Adn adnWrapper){
-		if (adnWrapper.isEmpty()) {
+	public ResponseEntity<String> isMutant(@NonNull @RequestBody Adn code){
+		Mutant mutant;
+		
+		if (code.isEmpty()) {
 			return new ResponseEntity<String>("ADN Vacío", HttpStatus.NO_CONTENT);		
 		}
-		if (!adnWrapper.isValid()) {
+		if (!code.isValid()) {
 			return new ResponseEntity<String>("ADN Inválido", HttpStatus.BAD_REQUEST);		
 		}
 		
-		this.mutant = adnWrapper.createtMutant(this.mutantService.isMutant(adnWrapper.getAdn()));
+		mutant = code.createtMutant(this.mutantService.isMutant(code.getCode()));
 		mutantService.save(mutant);
 		if (mutant.isMutant()){
-			return new ResponseEntity<String>(adnWrapper.toString() + " ES un mutante", HttpStatus.OK);		
+			return new ResponseEntity<String>(code.toString() + " ES un mutante", HttpStatus.OK);		
 		}
-		return new ResponseEntity<String>(adnWrapper.toString() + " NO ES un mutante", HttpStatus.FORBIDDEN);		
+		return new ResponseEntity<String>(code.toString() + " NO ES un mutante", HttpStatus.FORBIDDEN);		
 	}
 	
 	@GetMapping(value="/stats", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
