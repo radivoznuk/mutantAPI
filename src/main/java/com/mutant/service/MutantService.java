23e49af7afd96e398090cd4ff3830d2aacd628c8
radivoznuk <121996540+radivoznuk@users.noreply.github.com>
2023-01-06 04:07:45 -0300

    Refactoriza SERVICE y documenta

--- src/main/java/com/mutant/service/MutantService.java
+++ src/main/java/com/mutant/service/MutantService.java
@@ -16,17 +16,55 @@ public class MutantService {
 
 	public boolean isMutant(String[] adn) {
 		boolean isMutant = false;
-		int i, j;
-		i = j = 0;
+		int i = 0;
+		int j = 0;
 		while (!isMutant && i < adn.length) {
+			j = 0;
 			while (!isMutant && j < adn[i].length()) {
 				isMutant = verifyMutant(i, j, adn);
 				j++;
 			}
-			j = 0;
 			i++;
 		}
 		return isMutant;
+	}
+
+	private boolean verifyMutant(int i, int j, String[] adn) {
+		final int countEquals2BeMutant = 4;//se deja una constante que indica cuantos deben ser iguales para ser mutante
+		return (horizontalRepeated(i, j, adn, countEquals2BeMutant) || verticalRepeated(i, j, adn, countEquals2BeMutant) || diagonalRepeated(i, j, adn, countEquals2BeMutant));
+	}
+
+	private boolean verticalRepeated(int i, int j, String[] adn, int countEquals2BeMutant) {
+		int countEquals = 1;//cantidad repetidos
+		//mientras la fila a analizar mas la cantidad necesaria para ser mutante sea menor al tamaño de filas de la matriz
+		//y la cantidad de iguales sea menor a la cantidad de iguales necesarios para ser mutante
+		//y el caracter en la posicion (i,j) es igual al caracter en la posicion (i+countEquals,j)
+		while ((i+countEquals2BeMutant<adn.length) && (countEquals < countEquals2BeMutant) && (adn[i].charAt(j)==adn[i+countEquals].charAt(j))) {
+			countEquals++;
+		}
+		return (countEquals==countEquals2BeMutant);
+	}
+
+	private boolean horizontalRepeated(int i, int j, String[] adn, int countEquals2BeMutant) {
+		int countEquals = 1;//cantidad repetidos
+		//mientras la columna a analizar mas la cantidad necesaria para ser mutante sea menor al tamaño de columnas de la matriz
+		//y la cantidad de iguales sea menor a la cantidad de iguales necesarios para ser mutante
+		//y el caracter en la posicion (i,j) es igual al caracter en la posicion (i,j+countEquals)
+		while ((j+countEquals2BeMutant<adn[i].length()) && (countEquals < countEquals2BeMutant) && (adn[i].charAt(j)==adn[i].charAt(j+countEquals))) {
+			countEquals++;
+		}
+		return (countEquals==countEquals2BeMutant);
+	}
+
+	private boolean diagonalRepeated(int i, int j, String[] adn, int countEquals2BeMutant) {
+		int countEquals = 1;//cantidad repetidos
+		//mientras la columna y fila a analizar mas la cantidad necesaria para ser mutante sean menor al tamaño de columnas de la matriz
+		//y la cantidad de iguales sea menor a la cantidad de iguales necesarios para ser mutante
+		//y el caracter en la posicion (i,j) es igual al caracter en la posicion (i+countEquals,j+countEquals)
+		while ((i+countEquals2BeMutant<adn.length) && (j+countEquals2BeMutant<adn[i].length()) && (countEquals < countEquals2BeMutant) && (adn[i].charAt(j)==adn[i+countEquals].charAt(j+countEquals))) {
+			countEquals++;
+		}
+		return (countEquals==countEquals2BeMutant);
 	}
 
 	public void save(Mutant mutant) {
@@ -40,40 +78,4 @@ public class MutantService {
 	public int countMutants() {
 		return mutantRepository.countFindByIsMutant(true);
 	}
-
-	private boolean verifyMutant(int i, int j, String[] adn) {
-		return (horRepeated(i, j, adn) || verRepeated(i, j, adn) || diagRepeated(i, j, adn));
-	}
-
-	private boolean verRepeated(int i, int j, String[] adn) {
-		if (i < adn.length - 3)
-			return ((adn[i].charAt(j) == adn[i + 1].charAt(j)) && (adn[i + 1].charAt(j) == adn[i + 2].charAt(j))
-					&& (adn[i + 2].charAt(j) == adn[i + 3].charAt(j)));
-		else
-			return false;
-
-	}
-
-	private boolean horRepeated(int i, int j, String[] adn) {
-		if (j < adn[i].length() - 3)
-			return ((adn[i].charAt(j) == adn[i].charAt(j + 1)) && (adn[i].charAt(j + 1) == adn[i].charAt(j + 2))
-					&& (adn[i].charAt(j + 2) == adn[i].charAt(j + 3)));
-		else
-			return false;
-	}
-
-	private boolean diagRepeated(int i, int j, String[] adn) {
-		if ((j < adn[i].length() - 3) && (i < adn.length - 3))
-			return ((adn[i].charAt(j) == adn[i + 1].charAt(j + 1))
-					&& (adn[i + 1].charAt(j + 1) == adn[i + 2].charAt(j + 2))
-					&& (adn[i + 2].charAt(j + 2) == adn[i + 3].charAt(j + 3)));
-		else
-			return false;
-	}
-
-	public Object getCountMutants() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
