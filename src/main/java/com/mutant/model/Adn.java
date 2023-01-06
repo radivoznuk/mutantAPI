b53d2aa4ab24e6884418bf3344a236128ab8f71b
radivoznuk <121996540+radivoznuk@users.noreply.github.com>
2023-01-06 04:23:28 -0300

    Refactoriza cambiando nombre de la clase y moviendo de package

--- src/main/java/com/mutant/model/Adn.java
+++ src/main/java/com/mutant/model/Adn.java
@@ -1,23 +1,20 @@
-package com.mutant.wrapper;
+package com.mutant.model;
 
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
-
-import com.mutant.model.Mutant;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 @Data
 @RequiredArgsConstructor
-public class ADNWrapper {
+public class Adn {
 
 	private String[] adn;
 
 	public String toString() {
 		return String.join(",", this.adn);
 	}
-
 	
 	public boolean isEmpty() {
 		return (this.adn.length == 0);
@@ -30,7 +27,6 @@ public class ADNWrapper {
 	public boolean isValid() {
 		return (isNitrogenBase() || isValidStructure());
 	}
-
 
 	private boolean isNitrogenBase() {
 		//El ADN sólo puede contener las base nitrogenada del ADN sólo pueden ser: A, T, C y G
