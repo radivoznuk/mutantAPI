64054ba1fdd119c65732a886c7645edc61dde4b9
telecom <neradivoznuk@teco.com.ar>
2023-01-06 09:58:18 -0300

    Elimna dependencia Lombok

--- src/main/java/com/mutant/model/Stat.java
+++ src/main/java/com/mutant/model/Stat.java
@@ -2,11 +2,6 @@ package com.mutant.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
-
-@Data
-@RequiredArgsConstructor
 public class Stat {
 
 	@JsonProperty("Count Mutant ADN")
@@ -14,12 +9,35 @@ public class Stat {
 	@JsonProperty("Count Human ADN")
 	int countHumanAdn;
 	@JsonProperty("Ratio")
-	float ratio;	
-	
+	float ratio;		
 
 	public Stat(int countMutants, int countHumans) {
 		this.countMutantAdn = countMutants;
 		this.countHumanAdn = countHumans;
 		this.ratio = (countHumans==0 ? 1 : (float)countMutants / (float)countHumans);
 	}
+
+	public int getCountMutantAdn() {
+		return countMutantAdn;
+	}
+
+	public void setCountMutantAdn(int countMutantAdn) {
+		this.countMutantAdn = countMutantAdn;
+	}
+
+	public int getCountHumanAdn() {
+		return countHumanAdn;
+	}
+
+	public void setCountHumanAdn(int countHumanAdn) {
+		this.countHumanAdn = countHumanAdn;
+	}
+
+	public float getRatio() {
+		return ratio;
+	}
+
+	public void setRatio(float ratio) {
+		this.ratio = ratio;
+	}	
 }
