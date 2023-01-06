bd4ac5fb72cb6ee1c6d8674342192bf3a89cf028
radivoznuk <121996540+radivoznuk@users.noreply.github.com>
2023-01-06 16:34:59 -0300

    Implementa equals

--- src/main/java/com/mutant/model/Mutant.java
+++ src/main/java/com/mutant/model/Mutant.java
@@ -1,5 +1,7 @@
 package com.mutant.model;
 
+import java.util.Objects;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -15,8 +17,8 @@ public class Mutant {
 		this.isMutant = isMutant;
 	}
 
-	public Mutant() {}
-					
+	public Mutant() {
+	}
 
 	public String getAdn() {
 		return adn;
@@ -33,5 +35,22 @@ public class Mutant {
 	public void setMutant(boolean isMutant) {
 		this.isMutant = isMutant;
 	}
-	
+
+
+	 @Override
+	    public boolean equals(Object object) {
+	        if (this == object) {
+	            return true;
+	        }
+	        if (object == null) {
+	            return false;
+	        }
+	        if (!(object instanceof Mutant)) {
+	            return false;
+	        }
+	        Mutant that = (Mutant) object;
+	        return Objects.equals(this.adn, that.adn)
+	            && Objects.equals(this.isMutant, that.isMutant);
+	    }
+
 }
